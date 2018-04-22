11a93c21948793276183cbb4178dd0ef18b3a454
Kristopher Limpus <krislimpus@gmail.com>
2018-04-22 03:51:46 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,16 +1,18 @@
+# Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+# Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], #Top row
-  [3,4,5], #Middle row
-  [6,7,8], #Bottom row
-  [0,3,6], #Left column
-  [1,4,7], #Middle column
-  [2,5,8], #Right column
-  [0,4,8], #Left start diagonal
-  [2,4,6]  #Right start diagonal
+  [0,1,2], #top_row_win
+  [0,4,8], #left_diagonal_win
+  [0,3,6], #left_column_win
+  [1,4,7], #middle_column_win
+  [2,5,8], #right_column_win
+  [2,4,6], #right_diagonal_win
+  [3,4,5], #middle_row_win
+  [6,7,8] #bottom_row_win
 ]
 
 def won?(board)
@@ -44,7 +46,21 @@ def draw?(board)
   end
 end
 
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    return true
+  else
+    return false
+  end
+end
 
-
-
-
+def winner(board)
+  winning_board = won?(board)
+ if winning_board == false
+   return nil
+ elsif board[winning_board[0]].include?("X")
+   "X"
+ else board[winning_board[0]].include?("O")
+   "O"
+ end
+end
